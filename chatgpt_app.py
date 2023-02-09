f11fdbd9e0d5b6a0cb3cb7ff7d577b377987117b
Dr. Ernesto Lee <socrates73@gmail.com>
2023-02-09 15:58:24 -0800

    Update chatgpt_app.py

--- chatgpt_app.py
+++ chatgpt_app.py
@@ -51,14 +51,35 @@ def about_tab():
     st.header("About")
     st.write("Welcome to CAP4936: Special Topics in Data Analytics with Dr. Lee")
     st.write("This is an app that uses the OpenAI API's implementation of the ChatGPT model")
+def sentiment_tab():
+    user_query = st.text_input("Enter query here, to exit enter :q", "write a python class with a sample method?")
+    if user_query != ":q" or user_query != "":
+        response = ChatGPT(user_query)
+        return st.write(f"{user_query} {response}")
+    
+def get_sentiment(user_query):
+    # chatgpt = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=1000)
+    prompt = "Decide the sentiment of a passage as positive,neutral, or negative: " + user_query
+    chatgpt = openai.Completion.create(engine="text-davinci-003", prompt=input, max_tokens=1000)
+    # print(chatgpt.choices[0]['text'])
+    # print(type(chatgpt.choices[0]['text']))
+    return chatgpt.choices[0]["text"].replace("\n", "") # to remonve all the \n - Courtesy of Alex Z.
+  
+def get_image(prompt):
+    self.prompt = prompt
+    response = openai.Image.create(prompt=prompt, n=1,size="1024x1024")
+    image_url = response['data'][0]['url']
+    return image_url
 
 # Add the tabs to the app
 st.sidebar.title("Navigation")
-selected_tab = st.sidebar.radio("Select a tab", ["Code Help", "Concept Help", "About"])
+selected_tab = st.sidebar.radio("Select a tab", ["Code Help", "Concept Help", "About", "Sentiment"])
 
 if selected_tab == "Code Help":
     chat_tab()
 elif selected_tab == "Concept Help":
     instructions_tab()
+elif selected_tab == "Sentiment":
+    sentiment_tab()
 else:
     about_tab()
