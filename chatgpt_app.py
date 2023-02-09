b0e084e659582dfabdc5c3eed4c184639f88b685
Dr. Ernesto Lee <socrates73@gmail.com>
2023-02-08 19:17:37 -0800

    Update chatgpt_app.py

--- chatgpt_app.py
+++ chatgpt_app.py
@@ -39,5 +39,6 @@ def ChatGPT(user_query):
                                       )
     # response = completion.choices[0].text
     response = completion.choices[0]["text"].replace("\n", "")
+    formatted_response = textwrap.indent(text=response, prefix='    ')
     return response
 main()
